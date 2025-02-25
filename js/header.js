920a8f9228d3526aa401c5ab3a873b5c53e0e25d
felipe-coletti <felipecoletti2@gmail.com>
2025-02-25 08:56:00 -0300

    refactor: optimizing header effect

--- js/header.js
+++ js/header.js
@@ -1,13 +1,9 @@
-var header = document.getElementById('header')
+let header = document.getElementById('header')
 
 if (header.classList.contains('header-background') && window.scrollY === 0) {
     header.classList.remove('header-background')
 }
 
 window.addEventListener('scroll', function () {
-    if (window.scrollY > 0) {
-        header.classList.add('header-background')
-    } else {
-        header.classList.remove('header-background')
-    }
+    header.classList.toggle('header-background', window.scrollY > 0)
 })
